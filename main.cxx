36d4c13776e37cc76f859bc1f9b1fa8c6007a9bc
Steve McCoy <mccoyst@gmail.com>
2014-11-21 17:30:20 -0500

    Remove debugging of working stuff

--- main.cxx
+++ main.cxx
@@ -5,31 +5,6 @@
 GLint init_shaders();
 void load_texture(const char*);
 
-static void print_glerr(GLenum err, int line){
-#define x(e) case e: std::cerr << __FILE__ << ':' << line << ": " << #e << '\n'; break;
-	switch(err){
-	x(GL_INVALID_ENUM)
-	x(GL_INVALID_VALUE)
-	x(GL_INVALID_OPERATION)
-	x(GL_INVALID_FRAMEBUFFER_OPERATION)
-	x(GL_OUT_OF_MEMORY)
-	x(GL_STACK_UNDERFLOW)
-	x(GL_STACK_OVERFLOW)
-	}
-#undef x
-}
-
-static void check_gl(int line){
-	auto err = glGetError();
-	if(err == GL_NO_ERROR){
-		return;
-	}
-	print_glerr(err, line);
-	exit(1);
-}
-
-#define check() check_gl(__LINE__)
-
 int main(int argc, char *argv[]){
 	if(SDL_Init(SDL_INIT_VIDEO) < 0){
 		std::cerr << "SDL_Init: " << SDL_GetError() << '\n';
@@ -64,20 +39,15 @@ int main(int argc, char *argv[]){
 
 	std::cerr << glGetString(GL_VERSION) << '\n';
 	std::cerr << glGetString(GL_SHADING_LANGUAGE_VERSION) << '\n';
-	std::cerr << "residual errors:\n";
 	for(;;){
 		auto err = glGetError();
 		if(err == GL_NO_ERROR){
 			break;
 		}
-		print_glerr(err, __LINE__);
 	}
-	std::cerr << "...\n";
 
 	glEnable(GL_BLEND);
-	check();
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	check();
 
 	GLfloat verts[] = {
 		-0.5f,  0.5f, 0.0f, 0.0f,
@@ -90,54 +60,36 @@ int main(int argc, char *argv[]){
 
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
-	check();
 	glBindVertexArray(vao);
-	check();
 
 	GLuint vbo;
 	glGenBuffers(1, &vbo);
-	check();
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	check();
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-	check();
 
 	GLuint ebo;
 	glGenBuffers(1, &ebo);
-	check();
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	check();
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elems), elems, GL_STATIC_DRAW);
 
 	auto p = init_shaders();
-	check();
 
 	GLint pos = glGetAttribLocation(p, "position");
-	check();
 	glEnableVertexAttribArray(pos);
-	check();
 	glVertexAttribPointer(pos, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat[4]), 0);
-	check();
 
 	GLint texcoord = glGetAttribLocation(p, "texcoord");
-	check();
 	glEnableVertexAttribArray(texcoord);
-	check();
 	glVertexAttribPointer(texcoord, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat[4]), (void*)sizeof(GLfloat[2]));
-	check();
 
 	load_texture("meow.png");
 	glUniform1i(glGetUniformLocation(p, "sampo"), 0);
-	check();
 
 	glClearColor(0.5,0,0,1);
-	check();
 	glClear(GL_COLOR_BUFFER_BIT);
-	check();
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-	check();
 	SDL_GL_SwapWindow(win);
 
 	SDL_Delay(1000);
--- tex.cxx
+++ tex.cxx
@@ -4,32 +4,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2_image/SDL_image.h>
 
-static void print_glerr(GLenum err, int line){
-#define x(e) case e: std::cerr << __FILE__ << ':' << line << ": " << #e << '\n'; break;
-	switch(err){
-	x(GL_INVALID_ENUM)
-	x(GL_INVALID_VALUE)
-	x(GL_INVALID_OPERATION)
-	x(GL_INVALID_FRAMEBUFFER_OPERATION)
-	x(GL_OUT_OF_MEMORY)
-	x(GL_STACK_UNDERFLOW)
-	x(GL_STACK_OVERFLOW)
-	}
-#undef x
-}
-
-static void check_gl(int line){
-	auto err = glGetError();
-	if(err == GL_NO_ERROR){
-		return;
-	}
-	print_glerr(err, line);
-	exit(1);
-}
-
-#define check() check_gl(__LINE__)
-
-
 void load_texture(const char *fname){
 	auto surf = IMG_Load("meow.png");
 	if(!surf){
@@ -53,22 +27,15 @@ void load_texture(const char *fname){
 
 	GLuint tex;
 	glGenTextures(1, &tex);
-	check();
 	glBindTexture(GL_TEXTURE_2D, tex);
-	check();
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	check();
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	check();
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	check();
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	check();
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0,
 		fmt, GL_UNSIGNED_BYTE, surf->pixels);
-	check();
 
 	SDL_FreeSurface(surf);
 }
